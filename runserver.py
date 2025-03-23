d457182299725950399b91164009652181bfc2c6
KeironTJ <keirontj@gmail.com>
2025-03-23 22:17:37 +0000

    Unified runserver.py for development and production

--- runserver.py
+++ runserver.py
@@ -1,5 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
-app.run()
+if __name__ == '__main__':
+    if os.getenv('FLASK_ENV') == 'development':
+        app.run()
+    else:
+        # Add any production-specific settings, if needed
+        app.run()
