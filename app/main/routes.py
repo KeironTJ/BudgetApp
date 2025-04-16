cfbb94088673d51044ae3bbe5d6d529cee908439
KeironTJ <keirontj@gmail.com>
2025-04-16 22:32:10 +0100

    Add user profile route and template; implement 404 error page

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask_login import login_required, current_user
 from app import db, socketio
 from datetime import datetime, timedelta
+from app.models import User
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
@@ -16,6 +17,18 @@ def dashboard():
 
     return render_template('main/dashboard.html')
 
+
+@bp.route('/user_profile/<username>', methods=['GET', 'POST'])
+@login_required
+def user_profile(username):
+
+    user = User.query.filter_by(username=username).first()
+    
+    if not user:
+        return render_template('main/404.html'), 404
+
+    return render_template('main/user_profile.html', 
+                           user=user)
 
 
 
